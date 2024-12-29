c74eb9d11e4def5bacb18ce8c419eefc6ad745bc
Szymurai <szymuray@gmail.com>
2024-12-29 17:57:07 +0100

    feat: add drawing functionality for grid, pacman, and ship in HTML canvas for exercises 4-a and 4-b

---
+++ exercise4-b/drawing.js
@@ -0,0 +1,107 @@
+function draw_grid(ctx, minor, major, stroke, fill) {
+  minor = minor || 10;
+  major = major || minor * 5;
+  ctx.save();
+  stroke = stroke || "#00FF00";
+  fill = fill || "#009900";
+  ctx.strokeStyle = stroke;
+  ctx.fillStyle = fill;
+  let width = ctx.canvas.width;
+  let height = ctx.canvas.height;
+  for (var x = 0; x < width; x += minor) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height);
+    ctx.lineWidth = x % major == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+
+    if (x % major == 0) {
+      ctx.fillText(x, x, 10);
+    }
+  }
+
+  for (var y = 0; y < height; y += minor) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
+    ctx.lineWidth = y % major == 0 ? 0.5 : 0.25;
+    ctx.stroke();
+    if (y % major == 0) {
+      ctx.fillText(y, 0, y + 10);
+    }
+  }
+
+  ctx.restore();
+}
+
+// ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+function draw_pacman(ctx, xCoordinate, yCoordinate, radius, open) {
+  let width = ctx.canvas.width;
+  let height = ctx.canvas.height;
+  xCoordinate = xCoordinate || width / 2;
+  yCoordinate = yCoordinate || height / 2;
+  ctx.save();
+  fill = "yellow";
+  stroke = "#FF2222";
+  ctx.strokeStyle = stroke;
+  ctx.fillStyle = fill;
+  startAngle = open * 0.2 * Math.PI;
+  endAngle = (2 - open * 0.2) * Math.PI;
+  ctx.beginPath();
+  ctx.arc(xCoordinate, yCoordinate, radius, startAngle, endAngle);
+  ctx.lineTo(xCoordinate, yCoordinate);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.restore();
+}
+
+function draw_ship(ctx, radius, options) {
+  options = options || {};
+  let angle = (options.angle || 0.5 * Math.PI) / 2;
+  let curve = options.curve || 0.5;
+  ctx.save();
+  // Optionally draw a guide showing the collision radius
+  if (options.guide) {
+    ctx.strokeStyle = "white";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  }
+  ctx.lineWidth = options.lineWidth || 2;
+  ctx.strokeStyle = options.stroke || "white";
+  ctx.fillStyle = options.fill || "black";
+
+  // Draw the ship in three lines
+  ctx.beginPath();
+  ctx.moveTo(radius, 0);
+  ctx.lineTo(
+    Math.cos(Math.PI - angle) * radius,
+    Math.sin(Math.PI - angle) * radius
+  );
+  ctx.quadraticCurveTo(
+    radius * curve - radius,
+    0,
+    Math.cos(Math.PI + angle) * radius,
+    Math.sin(Math.PI + angle) * radius
+  );
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  if (options.guide) {
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.moveTo(-radius, 0);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(radius * curve - radius, 0, radius / 50, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  ctx.restore();
+}
